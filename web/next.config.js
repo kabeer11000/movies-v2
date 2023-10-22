fa23e99f5e816e12f56d0a7282480e428fe7bd1b
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-10-22 15:22:41 +0500

    iinitial

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true
+  },
   images: {
     domains: ['image.tmdb.org', 'yts.mx', 'rb.gy'],
   },
--- web/utils/requests.ts
+++ web/utils/requests.ts
@@ -2,7 +2,7 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
-    fetchMovies: genre => `https://yts.mx/api/v2/list_movies.json?genre=${genre}`,
+    fetchMovies: (genre: string) => `https://yts.mx/api/v2/list_movies.json?genre=${genre}`,
     fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
